ee487e361153a5448b8519d445cad40a2d4b2593
JuanRomer0 <juansebastianromerocepeda29@gmail.com>
2025-04-07 21:57:34 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const productList = [
+    "hodie",
+    "pants",
+    "merch bag",
+];
+
+const resultBox = document.querySelector(".results"); // Cambiado a .results
+const inputBox = document.querySelector(".search-bar"); // Cambiado a .search-bar
+
+const displayResults = function (results) {
+    if (results.length === 0) {
+        resultBox.innerHTML = '';
+        return;
+    }
+    
+    const resultHTML = results.map(function (product) {
+        return `<li onclick="selectInput(this)">${product}</li>`;
+    });
+    
+    resultBox.innerHTML = '<ul>' + resultHTML.join('') + '</ul>';
+}
+
+inputBox.onkeyup = function (e) {
+    let result = [];
+    const input = inputBox.value.toLowerCase();
+
+    if (input.length === 0) {
+        resultBox.innerHTML = '';
+        resultBox.style.display = 'none';
+        return;
+    }
+
+    result = productList.filter((product) => {
+        return product.toLowerCase().includes(input);
+    });
+
+    if (result.length > 0) {
+        resultBox.style.display = 'block';
+        displayResults(result);
+    } else {
+        resultBox.style.display = 'none';
+    }
+}
+
+function selectInput(element) {
+    inputBox.value = element.innerText;
+    resultBox.style.display = 'none';
+}
